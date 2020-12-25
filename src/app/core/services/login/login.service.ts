15db781cd0aedbfb179386831a1a38e658b92b39
eduardogb <eduardo.garciab@hotmail.com>
2020-12-24 19:21:59 -0600

    [ADD] Compiled files for deploy on ghp

--- src/app/core/services/login/login.service.ts
+++ src/app/core/services/login/login.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {LoginModel, LoginResponse} from "../../../models/Login.model";
-import {URL_ROOT_SERVICE} from "../../../../environments/environment";
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {LoginModel, LoginResponse} from '../../../models/Login.model';
+import {URL_ROOT_SERVICE} from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,8 @@ export class LoginService {
 
   readonly LOGIN_SERVICE: string = 'loginUsuario';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   logear(loginReques: LoginModel) {
     return this.http.post<LoginResponse>(URL_ROOT_SERVICE + this.LOGIN_SERVICE, loginReques);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true
 };
-
+export const URL_ROOT_SERVICE = 'http://localhost:8080/';
 export const URL_LOGIN: string = "http://localhost:8080/"
